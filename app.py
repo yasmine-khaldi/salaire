b5a0cd60683ccd9065b614a65c85a4a21528a463
yasmine-khaldi <khaldiyasmine27@gmail.com>
2024-05-04 21:56:16 +0200

    first commit

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('home.html')
-
+@app.route('/formulaire')
+def formulaire():
+    return render_template('formulaire.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
